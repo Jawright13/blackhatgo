c41e26b48bbcb101d450fb24f4dc6d190964a9b2
Joe <jawrig1313@gmail.com>
2025-02-03 18:33:55 -0500

    Scanner too fast

--- ch2/scanner.go
+++ ch2/scanner.go
@@ -18,7 +18,23 @@ func non_concurrent_scan(address string) {
 	}
 }
 
+// Too fast of a concurrent scanner
+func concurrent_scan_toofast(address string) {
+	for i := 1; i < 1024; i++ {
+		go func(j int) {
+			addr := fmt.Sprintf("%s:%d", address, i)
+			conn, err := net.Dial("tcp", addr)
+			if err != nil {
+				return
+			}
+			conn.Close()
+			fmt.Printf("%d open\n", i)
+		}(i)
+	}
+}
+
 func main() {
 	address := "scanme.nmap.org"
-	non_concurrent_scan(address)
+	//non_concurrent_scan(address)
+	concurrent_scan_toofast(address)
 }
